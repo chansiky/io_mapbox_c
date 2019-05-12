55db7f7e98146bc0d72d5d2dec56fa55c514c8af
chansiky <chansiky@gmail.com>
2019-05-12 18:19:05 -0500

    feat: less added

--- client/Sidebar.jsx
+++ client/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style_utility from './less/utility.less';
 
 const Sidebar = (props) => {
   return (
     <>
-      <section>
+      <section className={style_utility.background_color_a_hover}>
         <h2> Sidebar </h2>
       </section>
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,29 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            }
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
       }
     ]
   },
