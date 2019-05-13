d484859a3e6d1d20f4faf48ec112e9c3b9a68bdf
chansiky <chansiky@gmail.com>
2019-05-12 19:43:40 -0500

    feat: introduced THREE.js

--- client/Map.jsx
+++ client/Map.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect} from 'react';
 import {map_maker} from './mapbox';
 import {building_layer} from './mapbox/layer/building';
+import {gltf_layer} from './mapbox/layer/gltf';
+import {layer_adder} from './mapbox/layer_adder';
 
 const Map = (prop) => {
   const MAP_ELEM_ID = "map_elem";
   useEffect(() => {
     const map = map_maker(MAP_ELEM_ID);
     building_layer(map);
+    layer_adder(map, gltf_layer);
   });
 
   return (
