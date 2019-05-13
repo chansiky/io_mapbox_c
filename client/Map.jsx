f2008ab7087e20ed0444c961f2ccde43d2f981fc
chansiky <chansiky@gmail.com>
2019-05-12 19:47:15 -0500

    feat: webgl layer added to mapbox

--- client/Map.jsx
+++ client/Map.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {map_maker} from './mapbox';
 import {building_layer} from './mapbox/layer/building';
 import {gltf_layer} from './mapbox/layer/gltf';
+import {webgl_layer} from './mapbox/layer/webgl';
 import {layer_adder} from './mapbox/layer_adder';
 
 const Map = (prop) => {
@@ -10,6 +11,7 @@ const Map = (prop) => {
     const map = map_maker(MAP_ELEM_ID);
     building_layer(map);
     layer_adder(map, gltf_layer);
+    layer_adder(map, webgl_layer);
   });
 
   return (
